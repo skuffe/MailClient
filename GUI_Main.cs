65e48073d1008bbc34ad8cede0faf468c106fcb4
Danni Skov Hoeglund <skuffe@pwnz.dk>
2012-03-27 11:00:47 +0200

    Initial Build

---
+++ GUI_Main.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenPop.Mime;
+using OpenPop.Mime.Header;
+using OpenPop.Mime.Traverse;
+using OpenPop.Pop3;
+using OpenPop.Pop3.Exceptions;
+using OpenPop.Common.Logging;
+using Message = OpenPop.Mime.Message;
+
+
+namespace MailClient
+{
+    public partial class GUI_Main : Form
+    {
+        private readonly Pop3Client pop3Client;
+        private readonly Dictionary<int, Message> messages = new Dictionary<int, Message>();
+
+        public GUI_Main()
+        {
+            InitializeComponent();
+            pop3Client = new Pop3Client();
+        }
+
+        private void newMailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GUI_MailCreate MailForm = new GUI_MailCreate();
+            MailForm.Show();
+        }
+
+        private void optionsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            GUI_Option OptionForm = new GUI_Option();
+            OptionForm.Show();
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tbstNew_Click(object sender, EventArgs e)
+        {
+            GUI_MailCreate MailForm = new GUI_MailCreate();
+            MailForm.Show();
+        }
+
+        private void tbstGet1_Click_1(object sender, EventArgs e)
+        {
+            ReceiveMails();
+        }
+
+        private void ReceiveMails()
+        {
+            // Disable buttons while working
+            tsbtNew.Enabled = false;
+            tsbtGet.Enabled = false;
+            progressBar1.Value = 0;
+
+            try
+            {
+                if (pop3Client.Connected)
+                    pop3Client.Disconnect();
+                pop3Client.Connect(Properties.Settings.Default.popHost, Properties.Settings.Default.popPort, Properties.Settings.Default.popUseSSL);
+                pop3Client.Authenticate(Properties.Settings.Default.popUsername, Properties.Settings.Default.popPassword);
+                int count = pop3Client.GetMessageCount();
+                totalMessages1.Text =  "Total Messages: " + count.ToString();
+                messageTextBox.Text = "";
+                listMessages.Nodes.Clear();
+
+                int success = 0;
+                int fail = 0;
+                for (int i = count; i >= 1; i -= 1)
+                {
+                    // Check if the form is closed while we are working. If so, abort
+                    if (IsDisposed)
+                        return;
+
+                    // Refresh the form while fetching emails
+                    // This will fix the "Application is not responding" problem
+                    Application.DoEvents();
+
+                    try
+                    {
+                        Message message = pop3Client.GetMessage(i);
+
+                        // Add the message to the dictionary from the messageNumber to the Message
+                        messages.Add(i, message);
+
+                        // Create a TreeNode tree that mimics the Message hierarchy
+                        TreeNode node = new TreeNodeBuilder().VisitMessage(message);
+
+                        // Set the Tag property to the messageNumber
+                        // We can use this to find the Message again later
+                        node.Tag = i;
+
+                        // Show the built node in our list of messages
+                        listMessages.Nodes.Add(node);
+
+                        success++;
+                    }
+                    catch (Exception e)
+                    {
+                        DefaultLogger.Log.LogError(
+                            "TestForm: Message fetching failed: " + e.Message + "\r\n" +
+                            "Stack trace:\r\n" +
+                            e.StackTrace);
+                        fail++;
+                    }
+
+                    progressBar1.Value = (int)(((double)(count - i) / count) * 100);
+                }
+
+                MessageBox.Show(this, "Mail received!\nSuccesses: " + success + "\nFailed: " + fail, "Message fetching done");
+
+                if (fail > 0)
+                {
+                    MessageBox.Show(this,
+                                    "Since some of the emails were not parsed correctly (exceptions were thrown)\r\n" +
+                                    "please consider sending your log file to the developer for fixing.\r\n" +
+                                    "If you are able to include any extra information, please do so.",
+                                    "Help improve OpenPop!");
+                }
+            }
+            catch (InvalidLoginException)
+            {
+                MessageBox.Show(this, "The server did not accept the user credentials!", "POP3 Server Authentication");
+            }
+            catch (PopServerNotFoundException)
+            {
+                MessageBox.Show(this, "The server could not be found", "POP3 Retrieval");
+            }
+            catch (PopServerLockedException)
+            {
+                MessageBox.Show(this, "The mailbox is locked. It might be in use or under maintenance. Are you connected elsewhere?", "POP3 Account Locked");
+            }
+            catch (LoginDelayException)
+            {
+                MessageBox.Show(this, "Login not allowed. Server enforces delay between logins. Have you connected recently?", "POP3 Account Login Delay");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, "Error occurred retrieving mail. " + e.Message, "POP3 Retrieval");
+            }
+            finally
+            {
+                // Enable the buttons again
+                tsbtNew.Enabled = true;
+                tsbtGet.Enabled = true;
+                pop3Client.Disconnect();
+                progressBar1.Value = 100;
+            }
+        }
+
+        private void ListMessagesMessageSelected(object sender, TreeViewEventArgs e)
+        {
+            // Fetch out the selected message
+            Message message = messages[GetMessageNumberFromSelectedNode(listMessages.SelectedNode)];
+
+            // If the selected node contains a MessagePart and we can display the contents - display them
+            if (listMessages.SelectedNode.Tag is MessagePart)
+            {
+                MessagePart selectedMessagePart = (MessagePart)listMessages.SelectedNode.Tag;
+                if (selectedMessagePart.IsText)
+                {
+                    // We can show text MessageParts
+                    messageTextBox.Text = selectedMessagePart.GetBodyAsText();
+                }
+                else
+                {
+                    // We are not able to show non-text MessageParts (MultiPart messages, images, pdf's ...)
+                    messageTextBox.Text = "<<OpenPop>> Cannot show this part of the email. It is not text <<OpenPop>>";
+                }
+            }
+            else
+            {
+                // If the selected node is not a subnode and therefore does not
+                // have a MessagePart in it's Tag property, we genericly find some content to show
+
+                // Find the first text/plain version
+                MessagePart plainTextPart = message.FindFirstPlainTextVersion();
+                if (plainTextPart != null)
+                {
+                    // The message had a text/plain version - show that one
+                    messageTextBox.Text = plainTextPart.GetBodyAsText();
+                }
+                else
+                {
+                    // Try to find a body to show in some of the other text versions
+                    List<MessagePart> textVersions = message.FindAllTextVersions();
+                    if (textVersions.Count >= 1)
+                        messageTextBox.Text = textVersions[0].GetBodyAsText();
+                    else
+                        messageTextBox.Text = "<<OpenPop>> Cannot find a text version body in this message to show <<OpenPop>>";
+                }
+            }
+        }
+
+        private static int GetMessageNumberFromSelectedNode(TreeNode node)
+        {
+            if (node == null)
+                throw new ArgumentNullException("node");
+
+            // Check if we are at the root, by seeing if it has the Tag property set to an int
+            if (node.Tag is int)
+            {
+                return (int)node.Tag;
+            }
+
+            // Otherwise we are not at the root, move up the tree
+            return GetMessageNumberFromSelectedNode(node.Parent);
+        }
+    }
+
+    internal class TreeNodeBuilder : IAnswerMessageTraverser<TreeNode>
+    {
+        public TreeNode VisitMessage(Message message)
+        {
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            // First build up the child TreeNode
+            TreeNode child = VisitMessagePart(message.MessagePart);
+
+            // Then create the topmost root node with the subject as text
+            TreeNode topNode = new TreeNode(message.Headers.Subject, new[] { child });
+
+            return topNode;
+        }
+
+        public TreeNode VisitMessagePart(MessagePart messagePart)
+        {
+            if (messagePart == null)
+                throw new ArgumentNullException("messagePart");
+
+            // Default is that this MessagePart TreeNode has no children
+            TreeNode[] children = new TreeNode[0];
+
+            if (messagePart.IsMultiPart)
+            {
+                // If the MessagePart has children, in which case it is a MultiPart, then
+                // we create the child TreeNodes here
+                children = new TreeNode[messagePart.MessageParts.Count];
+
+                for (int i = 0; i < messagePart.MessageParts.Count; i++)
+                {
+                    children[i] = VisitMessagePart(messagePart.MessageParts[i]);
+                }
+            }
+
+            // Create the current MessagePart TreeNode with the found children
+            TreeNode currentNode = new TreeNode(messagePart.ContentType.MediaType, children);
+
+            // Set the Tag attribute to point to the MessagePart.
+            currentNode.Tag = messagePart;
+
+            return currentNode;
+        }
+    }
+
+}
---
+++ GUI_Option.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MailClient
+{
+    partial class GUI_Option
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI_Option));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.cbAuth = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPopPassword = new System.Windows.Forms.TextBox();
+            this.tbPopUsername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbPopPort = new System.Windows.Forms.TextBox();
+            this.tbPopHost = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.popUseSSL = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(482, 410);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelPassword);
+            this.tabPage1.Controls.Add(this.labelUsername);
+            this.tabPage1.Controls.Add(this.tbPassword);
+            this.tabPage1.Controls.Add(this.tbUsername);
+            this.tabPage1.Controls.Add(this.cbAuth);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.tbPort);
+            this.tabPage1.Controls.Add(this.tbHost);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(474, 384);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SMTP";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Enabled = false;
+            this.labelPassword.Location = new System.Drawing.Point(3, 131);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 10;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Enabled = false;
+            this.labelUsername.Location = new System.Drawing.Point(3, 105);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(55, 13);
+            this.labelUsername.TabIndex = 9;
+            this.labelUsername.Text = "Username";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Enabled = false;
+            this.tbPassword.Location = new System.Drawing.Point(83, 128);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 8;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Enabled = false;
+            this.tbUsername.Location = new System.Drawing.Point(83, 102);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 7;
+            // 
+            // cbAuth
+            // 
+            this.cbAuth.AutoSize = true;
+            this.cbAuth.Location = new System.Drawing.Point(6, 78);
+            this.cbAuth.Name = "cbAuth";
+            this.cbAuth.Size = new System.Drawing.Size(139, 17);
+            this.cbAuth.TabIndex = 6;
+            this.cbAuth.Text = "Requires Authentication";
+            this.cbAuth.UseVisualStyleBackColor = true;
+            this.cbAuth.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Server Settings";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(83, 52);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 3;
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(83, 26);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(100, 20);
+            this.tbHost.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SMTP Host";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.popUseSSL);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.tbPopPassword);
+            this.tabPage2.Controls.Add(this.tbPopUsername);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.tbPopPort);
+            this.tabPage2.Controls.Add(this.tbPopHost);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(474, 384);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "POP";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Username";
+            // 
+            // tbPopPassword
+            // 
+            this.tbPopPassword.Location = new System.Drawing.Point(83, 105);
+            this.tbPopPassword.Name = "tbPopPassword";
+            this.tbPopPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPopPassword.TabIndex = 18;
+            // 
+            // tbPopUsername
+            // 
+            this.tbPopUsername.Location = new System.Drawing.Point(83, 79);
+            this.tbPopUsername.Name = "tbPopUsername";
+            this.tbPopUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbPopUsername.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Server Settings";
+            // 
+            // tbPopPort
+            // 
+            this.tbPopPort.Location = new System.Drawing.Point(83, 52);
+            this.tbPopPort.Name = "tbPopPort";
+            this.tbPopPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPopPort.TabIndex = 14;
+            // 
+            // tbPopHost
+            // 
+            this.tbPopHost.Location = new System.Drawing.Point(83, 26);
+            this.tbPopHost.Name = "tbPopHost";
+            this.tbPopHost.Size = new System.Drawing.Size(100, 20);
+            this.tbPopHost.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Port";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Hostname";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 424);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // popUseSSL
+            // 
+            this.popUseSSL.AutoSize = true;
+            this.popUseSSL.Location = new System.Drawing.Point(6, 131);
+            this.popUseSSL.Name = "popUseSSL";
+            this.popUseSSL.Size = new System.Drawing.Size(68, 17);
+            this.popUseSSL.TabIndex = 21;
+            this.popUseSSL.Text = "Use SSL";
+            this.popUseSSL.UseVisualStyleBackColor = true;
+            // 
+            // GUI_Option
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 459);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GUI_Option";
+            this.Text = "GUI_Option";
+            this.Load += new System.EventHandler(this.GUI_Option_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.CheckBox cbAuth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPopPassword;
+        private System.Windows.Forms.TextBox tbPopUsername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbPopPort;
+        private System.Windows.Forms.TextBox tbPopHost;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox popUseSSL;
+    }
+}
